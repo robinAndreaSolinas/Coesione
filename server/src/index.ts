06c811921dc6bf9662385373f571b156aee13aed
Andrea Solinas <andrea.solinas.99@gmail.com>
2026-02-03 18:46:56 +0100

    change docker config

--- server/src/index.ts
+++ server/src/index.ts
@@ -32,6 +32,6 @@ if (fs.existsSync(staticDir)) {
   })
 }
 
-app.listen(PORT, () => {
-  console.log(`Server running on http://localhost:${PORT}`)
+app.listen(PORT, '0.0.0.0', () => {
+  console.log(`Server running on http://0.0.0.0:${PORT}`)
 })
